e053f3a6a30d88a7d62c62466ee24bfbeb203f7c
Joeri Smtis <joeri.smits@planet.nl>
2025-04-11 14:26:29 +0200

    feat: added updater

--- frontend/src-tauri/src/main.rs
+++ frontend/src-tauri/src/main.rs
@@ -1,32 +1,33 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::thread;
 use std::process::Command;
+use std::thread;
 
 fn main() {
-  tauri::Builder::default()
-    .setup(|_app| {
-      thread::spawn(|| {
-        #[cfg(target_os = "windows")]
-        {
-          Command::new("python")
-              .args(["../../backend/main.py"])
-              .spawn()
-              .expect("Failed to start Python backend");
-        }
+    tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .setup(|_app| {
+            thread::spawn(|| {
+                #[cfg(target_os = "windows")]
+                {
+                    Command::new("python")
+                        .args(["../../backend/main.py"])
+                        .spawn()
+                        .expect("Failed to start Python backend");
+                }
 
-        #[cfg(not(target_os = "windows"))]
-        {
-          Command::new("python3")
-              .args(["../../backend/main.py"])
-              .spawn()
-              .expect("Failed to start Python backend");
-        }
-      });
+                #[cfg(not(target_os = "windows"))]
+                {
+                    Command::new("python3")
+                        .args(["../../backend/main.py"])
+                        .spawn()
+                        .expect("Failed to start Python backend");
+                }
+            });
 
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,15 @@ import { useEffect, useState } from 'react'
 import Login from './components/login'
 import { supabase } from './lib/supabase'
 import Dashboard from './dashboard'
+import { check } from '@tauri-apps/plugin-updater';
+import { relaunch } from '@tauri-apps/plugin-process';
 
 function App() {
   const [user, setUser] = useState<any | null>(null)
 
   useEffect(() => {
+    runUpdater();
+
     // Fetch the session from localStorage on page load
     const saved = localStorage.getItem('supabase_session')
 
@@ -21,6 +25,38 @@ function App() {
       })
     }
   }, [])
+
+  
+  async function runUpdater() {
+    const update = await check();
+
+    if (update) {
+      console.log(
+        `found update ${update.version} from ${update.date} with notes ${update.body}`
+      );
+      let downloaded = 0;
+      let contentLength = 0;
+      // alternatively we could also call update.download() and update.install() separately
+      await update.downloadAndInstall((event) => {
+        switch (event.event) {
+          case 'Started':
+            contentLength = event.data.contentLength;
+            console.log(`started downloading ${event.data.contentLength} bytes`);
+            break;
+          case 'Progress':
+            downloaded += event.data.chunkLength;
+            console.log(`downloaded ${downloaded} from ${contentLength}`);
+            break;
+          case 'Finished':
+            console.log('download finished');
+            break;
+        }
+      });
+
+      console.log('update installed');
+      await relaunch();
+    }
+  }
 
 
   const handleLogin = async (loggedInUser: any) => {
